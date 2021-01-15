930f025be994f153f16d4f3f523e966a0acf92a1
Ciara Gilsenan <ciaragil98@gmail.com>
2021-01-15 17:05:16 +0000

    persona

--- persona.py
+++ persona.py
@@ -12,7 +12,15 @@ fw = open(file_name, 'a')
 personachat_file = cached_path(url)
 with open(personachat_file, "r", encoding="utf-8") as f:
     dataset = json.loads(f.read())
-    print(dataset['train'][:1][0]['utterances'][0].keys())
+    print(len(dataset['train'][2:3][0]['utterances'][5]['candidates']))
+    print(dataset['train'][2:3][0]['utterances'][1]['history'])
+    print(dataset['train'][2:3][0]['utterances'][2]['history'])
+    #print(dataset['train'][:2])
+    #print(len(dataset['train'][7:8][0]['utterances'][6]['history']))
+    #print(len(dataset['train'][19:20][0]['utterances']))
+    #print(len(dataset['valid']))
+    #print(dataset['train'][:1][0]['utterances'][0].keys())
+    """
     for i, persona in enumerate(dataset['train']):
         if i <= 3:
             label = "PERSONA " + str(i)
@@ -22,4 +30,5 @@ with open(personachat_file, "r", encoding="utf-8") as f:
             fw.write('\n')
             fw.writelines(persona['utterances'][0]['candidates'][:5])
             fw.write('\n')
+    """
         
