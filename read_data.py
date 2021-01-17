3ae31c026efb6a69eb9d8b6817dd9ebab27dd27b
Ciara Gilsenan <ciaragil98@gmail.com>
2021-01-17 17:33:52 +0000

    fixed bug in history list

--- read_data.py
+++ read_data.py
@@ -2,51 +2,65 @@ import pandas as pd
 import os
 import random
 
-celebs = "./celeb_data"
 """
 dataset = {'train': [[{'personality': [...], 'utterances': [{'candidates': [...], 
 	'history': [...]}, {'candidates': [...], 'history': [...]}]}]], 
 	'valid': ...}
-"""
-"""
+
+
     Procedure:
     personality sentences - may need to write them by hand, however only need around 5
     utterances: 7 dicts, each with 20 candidate sentences and anywhere between 1 - 13 history sentences
 
+    removing links:
+    import re
+    text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+
 """
+
+celebs = "./celeb_data"
+
 # testing reading in tweets
 df = pd.read_csv('celeb_data/ArianaGrande_tweets.csv')
 data = df.to_dict('records')
 
-#testing building dataset
+# testing building dataset
 test_persona = ['i was on tv', 'i have red hair', 'i love long jumpers', 'my favourite thing to do is cook', 
 'i have two dogs']
 history_appends = 2
 candidates_len = 20
 history_len = 13
 number_of_dicts = 7
-utterances = []
-candidates = []
 history = ['hi! how are you doing?']
+utterances = []
 
 for i in range(number_of_dicts):
-    for i in range(candidates_len):
-        candidates.append(data[i]['text'])
+    this_candidates = []
+    this_history = []
+    this_utterance = {'candidates': [], 'history': []}
+    for t in range(candidates_len):
+        this_candidates.append(data[t]['text'])
     
     data = data[candidates_len:]
-    
-    if i > 0:
-        this_history = []
-        for tweet in data[:history_appends]:
-            this_history.append(tweet['text'])
-        history.extend(this_history)
-    data = data[history_appends:]
 
-    this_utterance = {'candidates': candidates, 'history': history}
+    if i > 0:
+        for i in range(history_appends):
+            this_history.append(data[i]['text'])
+    
+        data = data[history_appends:]
+    
+    for it, x in enumerate(history):
+        this_history.insert(it, x)
+    
+    history = this_history
+
+    this_utterance['candidates'] = this_candidates
+    this_utterance['history'] = this_history
     utterances.append(this_utterance)
 
+
 this_persona = [{'personality': test_persona, 'utterances': utterances}]
-print(this_persona)
+
 
 
 dialog_dataset = {'train': [], 'valid': []}
